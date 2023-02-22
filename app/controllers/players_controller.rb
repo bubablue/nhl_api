b64e0e19b6f2785e480219fa485fdc263df89fb8
Jose <bubablue00@gmail.com>
2023-02-22 19:29:21 +0100

    undo: show definition

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -8,10 +8,7 @@ class PlayersController < ApplicationController
   end
 
   # GET /players/1 or /players/1.json
-  def show
-    players = Player.all.where(user_id: params[:user_id], player_id: params[:player_id])
-    render json: players
-  end
+  def show; end
 
   # GET /players/new
   def new
